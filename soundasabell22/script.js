0b788430b5a38f31c2ef14e8f157bde40984a9fa
macbeathdelamere <92879388+macbeathdelamere@users.noreply.github.com>
2022-04-20 11:13:46 +0100

    Update script.js

--- soundasabell22/script.js
+++ soundasabell22/script.js
@@ -1,3 +1,17 @@
+/*LANDING*/
+/*newsletter form*/
+
+function formSubmission(){
+
+	document.getElementById("formNewsletter").style.display = "none";
+    document.getElementById("confirm").style.display = "none";
+
+
+    alert("Thank you! Enjoy our weekly Newsletter");
+}
+
+/*LISTEN*/
+
 function play(){
     var audio = document.getElementById("audio");
     audio.play();
