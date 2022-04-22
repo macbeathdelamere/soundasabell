e03c599f43e4a0098df3f0c86917c7369889ddf7
lks-torres <100849626+lks-torres@users.noreply.github.com>
2022-04-22 23:57:33 +0100

    Update script.js

--- soundasabell22/script/script.js
+++ soundasabell22/script/script.js
@@ -13,3 +13,46 @@ function play(song){
 	audio.play();
 	console.log(audio);
     }
+
+//Learn
+function book(){
+	var name;
+	var email;
+	name=prompt("Please enter your name");
+	email = prompt("Please enter your email");
+    alert("Hi " +  name + " , your request was sent to your instructor and he will be in contact with you for more information");
+}
+
+//Contact
+function contactSubmit(){
+	console.log("check");	
+	var name = document.getElementById('username').value;
+	var address = document.getElementById('address').value;
+	var email = document.getElementById('email').value;
+	var message = document.getElementById('message').value;
+	var iSelection = document.getElementById('instrumentSelection').value;
+	var lSelection = document.getElementById('levelSelection').value;
+	
+if(name ===""){
+	alert("Please insert your name");
+}
+if(address ===""){
+	alert("Please insert your address");
+}
+if(email ===""){
+	alert("Please insert your email");
+}
+if(iSelection ==="--Select--"){
+	alert("Please select an instrument");
+}
+
+if(lSelection ==="--Select--"){
+	alert("Please select an Level of Experience");
+}
+if(message ===""){
+	alert("Please insert a message");
+else{
+	alert("Query received and we will contact you within 24 hours");
+}	
+
+}
